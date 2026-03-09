cc7f32b90b0a43d5350accf4f97f2144c7004227
robin-chauhan-2oo4 <robinchauhan15626@gmail.com>
2026-03-09 17:13:25 +0530

    Update API endpoint for dashboard stats

--- frontend/src/services/dashboard.api.js
+++ frontend/src/services/dashboard.api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getDashboardStats = async () => {
   const token = localStorage.getItem("token");
 
-  const res = await axios.get("http://localhost:5000/api/dashboard", {
+  const res = await axios.get("https://exam-buddy-a88x.onrender.com/api/dashboard", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
